396a1448e3d0d274340b548df541634eedafbfaf
Zyad416 <45890657+Zyad416@users.noreply.github.com>
2018-12-15 07:34:49 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'البرفيكس حق البوت'
+ 
+ 
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`LastCodes4Ever  `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
